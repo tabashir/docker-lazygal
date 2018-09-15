cf32766444c15690bdaa114e2efb353d29c749ed
Jez McKinley <jezm@ibcos.co.uk>
2018-09-15 23:28:17 +0100

    JM: debugging and explicit python call

--- run_conversion.sh
+++ run_conversion.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 cd "/gallery/files/"
+ls /gallery/files
+ls /gallery/files/photos
+ls /gallery/files/website
 
 if [ -d "photos" ]; then
   echo "photos folder exists, running conversion..."
   mkdir -p website
   cd website
-  /gallery/lazygal/lazygal.py --make-dir-zip --original --output-directory=/gallery/files/album /gallery/files/photos
+  python /gallery/lazygal/lazygal.py --make-dir-zip --original --output-directory=/gallery/files/website /gallery/files/photos
 fi
