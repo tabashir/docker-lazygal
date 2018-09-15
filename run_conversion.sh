0c9c72c3d09443388fd8fe8e38ecd4a28ea2501e
Jez McKinley <jezm@ibcos.co.uk>
2018-09-15 23:07:51 +0100

    JM: adding options and explicit output folder

--- run_conversion.sh
+++ run_conversion.sh
@@ -6,5 +6,5 @@ if [ -d "photos" ]; then
   echo "photos folder exists, running conversion..."
   mkdir -p website
   cd website
-  /gallery/lazygal/lazygal.py /gallery/files/photos
+  /gallery/lazygal/lazygal.py --make-dir-zip --original --output-directory=/gallery/files/album /gallery/files/photos
 fi
